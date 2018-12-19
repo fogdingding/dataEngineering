bc670ad62e7691d673e36bf702bc7362dbdc7814
fogdingding <fogdingding@gmail.com>
2018-12-20 01:42:17 +0800

    for add jump url.

--- routes.py
+++ routes.py
@@ -6,6 +6,7 @@ from dataEngineering import app
 def index():
    return render_template('index.html' , title='資料工程')
 
-@app.route('/search')
-def add_numbers():
-   pass
+@app.route('/search/<search_text>')
+def search(search_text):
+   print (search_text)
+   return render_template('test.html')
