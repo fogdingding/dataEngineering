ddeba0efbd40c850fab6616a43118c530a4bce06
fogdingding <fogdingding@gmail.com>
2019-01-06 23:29:22 +0800

    for add stock plot.

--- routes.py
+++ routes.py
@@ -1,6 +1,21 @@
 from flask import Flask, jsonify, render_template, request, url_for
 from dataEngineering import app
 from subprocess import Popen,PIPE,STDOUT,call
+import base64
+import pandas as pd
+from pandas_datareader import data as web
+import datetime
+import matplotlib.pyplot as plt 
+
+def save_image(stock):
+    start = datetime.datetime(2018,1,1)
+    end = datetime.date.today()
+    data = web.DataReader(stock, "yahoo", start,end)
+    c = data['Close']
+    plt.cla()
+    ax=c.plot(title=stock)
+    fig = ax.get_figure()
+    fig.savefig(stock+'.png')
 
 @app.route('/')
 # 這邊為主畫面
@@ -13,10 +28,27 @@ def index():
 @app.route('/search/<search_text>')
 def search(search_text):
    #gerp內建搜尋函數
-   search_string = 'grep '+search_text+' .\data\output.tsv -n'
+   search_string = 'grep '+search_text+' .\data\output.tsv'
    #這邊使用系統指令，並從中取出系統印出之字串
    proc=Popen(search_string, shell=True, stdout=PIPE, )
    output=proc.communicate()[0]
    #字串處理
    output_list = output.decode('utf-8').split("\n")
-   return render_template('search.html',search_text=output_list)
+   #測試用
+   with open('1101.tw.png','rb') as img_f:
+      img_stream = img_f.read()
+      img_stream = base64.b64encode(img_stream)
+      img_stream = str(img_stream,'utf8')
+   #測試用
+   return render_template('search.html',search_text=output_list,img_stream=img_stream)
+
+@app.route('/image/<id>')
+def get_image(id):
+   img_stream = ''
+   image_name = id+'.tw'
+   save_image(image_name)
+   with open(image_name+'.png','rb') as img_f:
+      img_stream = img_f.read()
+      img_stream = base64.b64encode(img_stream)
+      img_stream = str(img_stream,'utf8')
+   return img_stream
